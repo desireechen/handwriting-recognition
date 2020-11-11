f2fd7c23734fd0d47157f944dcec6d9edee062c2
desiree_chen <desiree_chen@aiap.sg>
2020-11-11 22:52:55 +0800

    Initial commit

---
+++ project/text_recognizer/models/__init__.py
@@ -0,0 +1,8 @@
+"""Model modules."""
+from .character_model import CharacterModel
+
+from .line_model import LineModel
+
+from .line_model_ctc import LineModelCtc
+
+from .line_detector_model import LineDetectorModel
